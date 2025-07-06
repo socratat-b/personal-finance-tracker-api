fa243b4fa45458de8055e1c60cea4111a8256382
tat <bedisscottandrew@gmail.com>
2025-07-06 22:56:53 +0800

    chore: added constants for categories and error message.

---
+++ utils/constants.js
@@ -0,0 +1,16 @@
+export const CATEGORIES = [
+  "food",
+  "transport",
+  "entertainment",
+  "bills",
+  "shopping",
+  "health",
+  "other",
+];
+
+export const ERROR_MESSAGES = {
+  EXPENSE_NOT_FOUND: "Expense not found",
+  INVALID_AMOUNT: "Amount must be a positive number",
+  INVALID_CATEGORY: "Invalid category",
+  MISSING_REQUIRED_FIELD: "Missing required field",
+};
